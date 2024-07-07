d8fa26f8bd8844fb86501c2605006ebb3d9c50a4
Ion-Gavrilencu <157795149+Ion-Gavrilencu@users.noreply.github.com>
2024-07-07 13:16:55 +0300

    modificari finale

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,9 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+import Notiflix from 'notiflix';
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('promiseForm');
+
+  if (form) {
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const delay = Number(form.delay.value);
+      const step = Number(form.step.value);
+      const amount = Number(form.amount.value);
+
+      for (let i = 0; i < amount; i++) {
+        const currentDelay = delay + i * step;
+        createPromise(i + 1, currentDelay)
+          .then(({ position, delay }) => {
+           Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          })
+          .catch(({ position, delay }) => {
+             Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          });
+      }
+    });
   } else {
-    // Reject
+    console.error('Form element not found');
   }
-}
+
+  function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
+});
+
 
